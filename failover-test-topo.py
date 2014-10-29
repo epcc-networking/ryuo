a29f9467521fb4ca7f6aef32a30cee348695266a
Shaoyu Zhang <zsy056@gmail.com>
2014-10-29 20:03:40 +0900

    new test topo

--- failover-test-topo.py
+++ failover-test-topo.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python2
 
-from mininet.net import Mininet
 from mininet.node import OVSSwitch
-from mininet.node import Host
 from mininet.topo import Topo
 from mininet.cli import CLI
 from mininet.net import Mininet
 from mininet.node import RemoteController
 from mininet.log import setLogLevel
-from mininet.link import Intf
+
 
 class FailoverTestTopo(Topo):
     """
     Simple topology to test failover group
+
                         10.0.2.2  10.0.3.1
                      ,----------s2----------,
     10.0.1.2 10.0.1.1|10.0.2.1      10.0.3.2|10.0.6.1 10.0.6.2
@@ -26,13 +25,13 @@ class FailoverTestTopo(Topo):
         Topo.__init__(self)
 
         h1 = self.addHost(
-                'h1',
-                ip   = '10.0.1.2/24',
-                defaultRoute = 'via 10.0.1.1')
+            'h1',
+            ip='10.0.1.2/24',
+            defaultRoute='via 10.0.1.1')
         h2 = self.addHost(
-                'h2',
-                ip = '10.0.6.2/24',
-                defaultRoute = 'via 10.0.6.1')
+            'h2',
+            ip='10.0.6.2/24',
+            defaultRoute='via 10.0.6.1')
 
         s1 = self.addSwitch('s1', protocols='OpenFlow13')
         s2 = self.addSwitch('s2', protocols='OpenFlow13')
@@ -41,22 +40,24 @@ class FailoverTestTopo(Topo):
 
         self.addLink(h1, s1)
         self.addLink(h2, s4)
-        self.addLink(s1, s2) 
-        self.addLink(s2, s4) 
-        self.addLink(s1, s3) 
-        self.addLink(s3, s4) 
+        self.addLink(s1, s2)
+        self.addLink(s2, s4)
+        self.addLink(s1, s3)
+        self.addLink(s3, s4)
+
 
 def run():
     OVSSwitch.setup()
     setLogLevel('debug')
 
     net = Mininet(
-            topo        = FailoverTestTopo(),
-            switch      = OVSSwitch,
-            controller  = RemoteController)
+        topo=FailoverTestTopo(),
+        switch=OVSSwitch,
+        controller=RemoteController)
     net.start()
     CLI(net)
     net.stop()
+
 
 if __name__ == '__main__':
     run()
---
+++ keep_forwarding_test_topo.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python2
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+from mininet.net import Mininet
+from mininet.node import OVSSwitch, RemoteController
+from mininet.topo import Topo
+
+OFP = 'OpenFlow13'
+
+
+class KeepForwardingTestTopo(Topo):
+    """
+    Simple topology to test keep forwarding implementation
+
+               2.1     2.2   3.2   3.1
+               ,----------s4---------,
+               |                     |
+    1.2     1.1|4.1     4.2  7.1  7.2|8.1   8.2
+    h1---------s1----------s3--------s5------h2
+               |           |
+               |           |
+               '----s2-----'
+             5.1 5.2  6.1  6.2
+    """
+
+    def __init__(self):
+        Topo.__init__(self)
+
+        h1 = self.addHost('h1',
+                          ip='10.0.1.2/24',
+                          defaultRoute='via 10.0.1.1')
+        h2 = self.addHost('h2',
+                          ip='10.0.8.2/24',
+                          defaultRoute='via 10.0.8.1')
+
+        s = [self.addSwitch('s%d' % i + 1, protocols=OFP) for i in range(0, 5)]
+
+        self.addLink(h1, s[1])
+        self.addLink(h2, s[5])
+
+        graph = [[0, 1, 1, 1, 0],
+                 [1, 0, 1, 0, 0],
+                 [1, 1, 0, 0, 1],
+                 [1, 0, 0, 0, 1],
+                 [0, 0, 1, 1, 0]]
+
+        for i in range(0, 5):
+            for j in range(0, i):
+                if graph[i][j] == 1:
+                    self.addLink(s[i], s[j])
+
+
+def run():
+    OVSSwitch.setup()
+    setLogLevel('debug')
+
+    net = Mininet(topo=KeepForwardingTestTopo(),
+                  switch=OVSSwitch,
+                  controller=RemoteController)
+    net.start()
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    run()
